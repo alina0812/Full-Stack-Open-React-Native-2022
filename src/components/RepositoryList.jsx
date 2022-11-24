797623fce87f89a9d01e3ecb14739ac2f05ca1c4
Alina Brockmann <brockmannalina@web.de>
2022-11-24 10:07:21 +0100

    Exercise 10.23: sorting the reviewed repositories list

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,20 +1,40 @@
+import React, { useState } from 'react';
 import { FlatList, View, StyleSheet, Pressable } from 'react-native';
+import { Button, Menu, Divider, Provider } from 'react-native-paper';
+import { AntDesign } from '@expo/vector-icons';
 import RepositoryItem from './RepositoryItem';
 import useRepositories from '../hooks/useRepositories';
 import { useNavigate } from 'react-router-native';
+import theme from '../theme';
+import Text from './Text';
 
 const styles = StyleSheet.create({
   separator: {
     height: 10,
   },
+  menuView: {
+      paddingVertical: 5,
+      flexDirection: 'row',
+      justifyContent: 'center',
+  },
+  menu: {
+    color: theme.colors.textPrimary,
+    fontSize: theme.fontSizes.body,
+    fontWeight: theme.fontWeights.semibold,
+    paddingHorizontal: 10,
+}
 });
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
 const RepositoryList = () => {
 
-  const { repositories } = useRepositories();
   let navigate = useNavigate();
+  const [visible, setVisible] = useState(false);
+  const [item, setItem] = useState("Latest repositories");
+  const openMenu = () => setVisible(true);
+  const closeMenu = () => setVisible(false);
+  const { repositories } = useRepositories(item);
 
   const repositoryNodes = repositories
     ? repositories.edges.map(edge => edge.node)
@@ -27,11 +47,30 @@ const RepositoryList = () => {
     );
 
   return (
-    <FlatList
-      data={repositoryNodes}
-      renderItem={renderItem}
-      ItemSeparatorComponent={ItemSeparator}
-    />
+    <Provider>
+          <View style={styles.menuView}>
+            <Menu
+            visible={visible}
+            onDismiss={closeMenu}
+            anchor={<View><Button onPress={openMenu}>
+                      <Text style={styles.menu}>{item}</Text>
+                      <AntDesign name="caretdown" size={24} color="black" />
+                    </Button></View>}
+            style={styles.menu}>
+              <Menu.Item onPress={() => {}} title="Select an item..." disabled="true" />
+              <Divider />
+              <Menu.Item onPress={() => {setItem("Latest repositories")}} title="Latest repositories" />
+              <Menu.Item onPress={() => {setItem("Highest rated repositories")}} title="Highest rated repositories" />
+              <Menu.Item onPress={() => {setItem("Lowest rated repositories")}} title="Lowest rated repositories" />
+            </Menu>
+          </View>
+          <FlatList
+          data={repositoryNodes}
+          renderItem={renderItem}
+          ItemSeparatorComponent={ItemSeparator}
+          />
+        </Provider>
+        
   );
 };
 
--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,8 +1,26 @@
 import { useQuery } from '@apollo/client';
 import { GET_REPOSITORIES } from '../graphql/queries';
 
-const useRepositories = () => {
-    const { data, error, loading } = useQuery(GET_REPOSITORIES, {fetchPolicy: 'cache-and-network'});
+const useRepositories = (item) => {
+    console.log(item);
+    let orderBy = null;
+    let orderDirection = null;
+    if(item == "Latest repositories") {
+      console.log("1")
+      orderBy = "CREATED_AT";
+      orderDirection = "DESC";
+    } else if (item == "Highest rated repositories") {
+      console.log("2")
+      orderBy = "RATING_AVERAGE";
+      orderDirection = "DESC";
+    } else if (item == "Lowest rated repositories") {
+      console.log("3")
+      orderBy = "RATING_AVERAGE";
+      orderDirection = "ASC";
+    }
+    console.log(orderBy);
+    console.log(orderDirection);
+    const { data, error, loading } = useQuery(GET_REPOSITORIES, {fetchPolicy: 'cache-and-network', variables: {"orderBy": orderBy, "orderDirection": orderDirection}});
 
   return { repositories: data && data.repositories, error, loading };
 };
